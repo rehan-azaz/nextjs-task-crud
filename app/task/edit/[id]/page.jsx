cee02cc70180336a9dc51c8897cbc046a4fc72dd
Rehan Azaz <dev.rehan.azaz@gmail.com>
2023-09-27 06:36:10 +0500

    CRUD UI

---
+++ app/task/edit/[id]/page.jsx
@@ -0,0 +1,8 @@
+import EditTaskForm from "@/components/EditTaskForm";
+import React from "react";
+
+const EditTask = () => {
+  return <EditTaskForm />;
+};
+
+export default EditTask;
---
+++ app/task/page.jsx
@@ -0,0 +1,12 @@
+import TaskList from "@/components/TaskList";
+import React from "react";
+
+const Tasks = () => {
+  return (
+    <>
+      <TaskList priority={"any"} />
+    </>
+  );
+};
+
+export default Tasks;
